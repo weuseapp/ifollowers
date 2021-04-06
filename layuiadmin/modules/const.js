c2e2512c72f8b2c3c00e81650233fb17f0fed0e9
luo863 <68792409+luo863@users.noreply.github.com>
2021-04-06 18:16:45 +0800

    update 2.0

--- layuiadmin/modules/const.js
+++ layuiadmin/modules/const.js
@@ -1,22 +1,2 @@
-layui.define(function (e) {
-    e('const', {
-        // 商品状态，0：待上架，1：已上架，2：已下架
-        STATUS_WAITING: 0,
-        STATUS_ENABLE: 1,
-        STATUS_DISABLE: 2,
-        // 支付方式，1：金币，2：金钱
-        PAY_TTPE_COINS: 1,
-        PAY_TYPE_MONEY: 2,
-        // 商品类型，1：金币，2：任务，3：订阅
-        PRODUCT_TYPE_COINS: 1,
-        PRODUCT_TYPE_TASK: 2,
-        PRODUCT_TYPE_SUB: 3,
-        // 其他类型，-1：未知，0：点赞，1：加粉，2：观看，3：金币，默认为0
-        OTHER_TYPE_UNKNOWN: -1,
-        OTHER_TYPE_LIKE: 0,
-        OTHER_TYPE_FOLLOWER: 1,
-        OTHER_TYPE_VIEW: 2,
-        OTHER_TYPE_COINS: 3,
-        OTHER_TYPE_GOLD_FOLLOWER: 4,
-    });
-});
+/** layui-admin.std-v1.4.0 LPPL License By https://www.layui.com/layuiadmin/ */
+ ;"use strict";layui.define(function(T){T("const",{STATUS_WAITING:0,STATUS_ENABLE:1,STATUS_DISABLE:2,PAY_TTPE_COINS:1,PAY_TYPE_MONEY:2,PRODUCT_TYPE_COINS:1,PRODUCT_TYPE_TASK:2,PRODUCT_TYPE_SUB:3,OTHER_TYPE_UNKNOWN:-1,OTHER_TYPE_LIKE:0,OTHER_TYPE_FOLLOWER:1,OTHER_TYPE_VIEW:2,OTHER_TYPE_COINS:3,OTHER_TYPE_GOLD_FOLLOWER:4})});
